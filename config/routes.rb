8710cd6106c8b10300b33014c6fe5acea714e2bf
Jack Bauer <bauer.jackw@gmail.com>
2018-12-06 15:09:51 -0800

    adds ability to login and register account

--- config/routes.rb
+++ config/routes.rb
@@ -12,10 +12,19 @@ Rails.application.routes.draw do
 
   resources :orders, only: [:create, :show]
 
+  resources :users, only: [:create, :new]
+    get '/login' => 'sessions#new'
+    post '/login' => 'sessions#create'
+    get '/logout' => 'sessions#destroy'
+
+    get '/signup' => 'users#new'
+    post '/users' => 'users#create'
+
   namespace :admin do
     root to: 'dashboard#show'
     resources :products, except: [:edit, :update, :show]
     resources :categories
+    
   end
 
   # The priority is based upon order of creation: first created -> highest priority.
